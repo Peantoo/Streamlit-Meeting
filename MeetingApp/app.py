c7f564ee1876881c9673031030e5551071d6d560
Peantoo <dusand@gmail.com>
2023-05-10 11:21:47 -0400

    Update app.py
    Completely rewrote the code. This version uses LangChain summarizer instead of using a version of GPT-3.5. We'll see if it performs as well. I also am using Streamlit for the interface as it's simpler for debugging the backend. I don't want to spend hours debugging the interface yet. I'm also going to add Whisper as the speech to text algorithm, but I need to get those details ironed out first.

--- MeetingApp/app.py
+++ MeetingApp/app.py
@@ -1,11 +1,19 @@
-import requests
-from transformers import pipeline
 import os
+from io import BytesIO
+from transformers import pipeline
+import requests
 from flask import Flask, request, jsonify
+from langchain import OpenAI, LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.text_splitter import CharacterTextSplitter
+import streamlit as st
 
 app = Flask(__name__)
 
+# Set API Key
 APIKEY = os.environ.get("HF_API_KEY")
+os.environ['OPENAI_API_KEY'] = APIKEY
+
 # Speech-to-text
 API_URL = "https://api-inference.huggingface.co/models/jonatasgrosman/wav2vec2-large-xlsr-53-english"
 headers = {"Authorization": APIKEY}
@@ -15,7 +23,8 @@ def query(audio_data):
     return response.json()
 
 # Summarization
-summarizer = pipeline("summarization", model="philschmid/bart-large-cnn-samsum")
+llm = OpenAI(temperature=0)
+summarizer = LLMChain(llm=llm, prompt=PromptTemplate("summarize {text}"), output_key="summary")
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -29,15 +38,40 @@ def upload():
     # Read the file into memory
     audio_data = BytesIO(file.read())
 
+    # Placeholder block for Speech-to-Text code
     transcription_output = query(audio_data.getvalue())
     transcription_text = transcription_output['text']
-    summary_output = summarizer(transcription_text)
-    summary_text = summary_output[0]['summary_text']
+
+    summary_output = summarizer.run(text=transcription_text)
+    summary_text = summary_output['summary']
 
     return jsonify({'transcription': transcription_text, 'summary': summary_text}), 200
-    
+
+# Streamlit Interface
+def streamlit_interface():
+    st.title('🎙️ Speech-to-Text Summarizer')
+    st.write("Upload an audio file for transcription and summarization.")
+    audio_file = st.file_uploader("Choose an audio file", type=['wav', 'mp3', 'ogg'])
+
+    if audio_file:
+        audio_data = BytesIO(audio_file.read())
+
+        # Placeholder block for Speech-to-Text code
+        transcription_output = query(audio_data.getvalue())
+        transcription_text = transcription_output['text']
+
+        summary_output = summarizer.run(text=transcription_text)
+        summary_text = summary_output['summary']
+
+        st.write('**Transcription:**')
+        st.write(transcription_text)
+
+        st.write('**Summary:**')
+        st.write(summary_text)
+
 if __name__ == '__main__':
     try:
-        app.run(debug=True)
+        # Run Streamlit interface
+        streamlit_interface()
     except Exception as e:
         print(f"Error: {e}")
