d8a1f33237c1c3321b92bb5412bea85c4d26ad4b
Peantoo <dusand@gmail.com>
2023-05-12 12:28:07 -0400

    Update streamlit_app.py

--- MeetingApp/streamlit_app.py
+++ MeetingApp/streamlit_app.py
@@ -8,20 +8,13 @@ from streamlit_webrtc import (
 )
 from io import BytesIO
 import requests
+from Meeting-App.st_custom_components import st_audiorec
 
 # URL of your the Flask app
 FLASK_APP_URL = "https://meeting-summarizer.herokuapp.com/upload"
 
 # Any code needed for the Streamlit app.
 
-class AudioRecorder(AudioProcessorBase):
-    def __init__(self):
-        super().__init__()
-        self.audio_buffer = BytesIO()
-
-    def recv(self, frame: av.AudioFrame) -> av.AudioFrame:
-        frame.to_ndarray().tobytes(stream=self.audio_buffer)
-        return None
 
 def main():
     st.title('🎙️ Meeting Summarizer')
@@ -30,42 +23,42 @@ def main():
     if "recording" not in st.session_state:
         st.session_state.recording = False
 
-    webrtc_ctx = webrtc_streamer(key="audio", mode=WebRtcMode.SENDONLY, client_settings=ClientSettings(rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}], "trickle": True}, media_stream_constraints={"video": False, "audio": True},), audio_processor_factory=AudioRecorder)
-
+    audio_data = None
     start_button = st.button("Start Meeting")
     stop_button = st.button("End Meeting")
     
     if start_button and not st.session_state.recording:
         st.session_state.recording = True
-        
+        audio_data = st_audiorec()
+
     if stop_button:
-        if webrtc_ctx.state.playing:
-            webrtc_ctx.stop_all()
-            webrtc_ctx.audio_processor.audio_buffer.seek(0)
-            audio_data = webrtc_ctx.audio_processor.audio_buffer
+        if st.session_state.recording:
+            st.session_state.recording = False
 
-            # Send the recorded audio to the Flask API
-            response = requests.post(
-                FLASK_APP_URL,
-                files={"file": (f"audio.wav", audio_data, "audio/wav")},
-            )
+            if audio_data is not None:
+                # Send the recorded audio to the Flask API
+                response = requests.post(
+                    FLASK_APP_URL,
+                    files={"file": ("audio.wav", audio_data, "audio/wav")},
+                )
 
-            if response.status_code == 200:
-                result = response.json()
-                transcription_text = result["transcription"]
-                summary_text = result["summary"]
+                if response.status_code == 200:
+                    result = response.json()
+                    transcription_text = result["transcription"]
+                    summary_text = result["summary"]
 
-                st.write('**Transcription:**')
-                st.write(transcription_text)
+                    st.write('**Transcription:**')
+                    st.write(transcription_text)
 
-                st.write('**Summary:**')
-                st.write(summary_text)
+                    st.write('**Summary:**')
+                    st.write(summary_text)
 
-            else:
-                st.error(f"Error processing the audio. Please try again. (Error: {response.status_code})")
+                else:
+                    st.error(f"Error processing the audio. Please try again. (Error: {response.status_code})")
 
         else:
             st.warning("Please start the meeting first before stopping it.")
+
     if st.session_state.recording:
         st.warning("Meeting is in progress...")
 
